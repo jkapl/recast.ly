9e823140112124014f40c87f1da6d9068f08adc0
jkapl <joelkaplan1@gmail.com>
2019-06-10 20:37:45 -0700

    got some stuff working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
-var App = () => (
+import VideoList from './VideoList.js';
+
+var App = (props) => (
+  
   <div>
+ 
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
         <div><h5><em>search</em> view goes here</h5></div>
@@ -10,7 +14,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em> <VideoList videoArray={props.list}/></h5></div>
       </div>
     </div>
   </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,16 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div>
+      <h5>
+        <em>videoListEntry</em>
+        {props.videoArray.map(item =>
+          <VideoListEntry entry={item} />
+        )}
+      </h5>
+    </div>
+
   </div>
 );
 
